934d75b6eaeccbe84c0c1550aa96c2a9494a6ef4
VextoN <38920805+VextoN@users.noreply.github.com>
2018-05-02 18:31:24 +0100

    Add files via upload

---
+++ Board.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include "consolecolors.h"
+#ifndef BOARD_H
+#define BOARD_H
+
+using namespace std;
+
+class Board
+{
+public:
+	Board(int x, int y);
+	void Mostrador() const;
+	void Principal();	
+	void InsereHorizontal(int posicaoX, int posicaoY, string palavra);
+	void InsereVertical(int posicaoX, int posicaoY, string palavra);
+	bool VerificaHorizontal(int posicaoX, int posicaoY, string palavra);
+	bool VerificaVertical(int posicaoX, int posicaoY, string palavra);
+
+private:
+	vector<vector<char>> board;
+	int x, y;
+};
+
+#endif
