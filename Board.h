18cc2644c3b09093768acc63c0a7828246abb20e
VextoN <38920805+VextoN@users.noreply.github.com>
2018-05-03 11:40:47 +0100

    Update Board.h

--- Board.h
+++ Board.h
@@ -13,18 +13,14 @@ public:
 	Board(int x, int y);
 	void display() const;
 	void getWord();
-	void InsereHorizontal(int posicaoX, int posicaoY, string palavra);
-	void InsereVertical(int posicaoX, int posicaoY, string palavra);
-	bool VerificaHorizontal(int posicaoX, int posicaoY, string palavra);
-	bool VerificaVertical(int posicaoX, int posicaoY, string palavra);
-
-	bool VerificaCursor(int posicaoX, int posicaoY, char direcao, string palavra); //Ve se pode por a palavra
-	void InsereCursor(int posicaoX, int posicaoY, char direcao, string palavra); //mete
-	void MudaCursor(char letra); //altera no board a letra com base na posicao do cursor
-	char VeCursor(); //ve que letra esta na posicao do cursor
-	int boardmaincoord(); //da a coordenada maxima do board na direcao correspondente
+	bool Verify(int posX, int posY, char direction, string palavra); //Ve se pode por a palavra
+	void Insert(int posX, int posY, char direction, string palavra); //mete
+	void ChangeChar(char letra); //altera no board a letra com base na posicao do cursor
+	char ShowChar(); //ve que letra esta na posicao do cursor
+	int BoardMainCoord(); //da a coordenada maxima do board na direcao atual
 private:
 	vector<vector<char>> board;
 	int x, y;
 	Cursor Cursor;
 };
+
