8236a044f65b6053f3930c0583932c5e06b13174
MOAAS <38762849+MOAAS@users.noreply.github.com>
2018-05-03 14:54:19 +0100

    adicionei const

--- Board.h
+++ Board.h
@@ -4,6 +4,8 @@
 #include <ctime>
 #include <cstdlib>
 #include <string>
+#include "Cursor.h"
+#include <map>
 
 using namespace std;
 
@@ -16,11 +18,11 @@ public:
 	bool Verify(int posX, int posY, char direction, string palavra); //Ve se pode por a palavra
 	void Insert(int posX, int posY, char direction, string palavra); //mete
 	void ChangeChar(char letra); //altera no board a letra com base na posicao do cursor
-	char ShowChar(); //ve que letra esta na posicao do cursor
-	int BoardMainCoord(); //da a coordenada maxima do board na direcao atual
+	char ShowChar() const; //ve que letra esta na posicao do cursor
+	int CoordLimit() const; //da a coordenada maxima do board na direcao atual
 private:
 	vector<vector<char>> board;
 	int x, y;
+	map<string, string> words;
 	Cursor Cursor;
 };
-
