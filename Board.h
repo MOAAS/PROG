0dcf60d4f48b0deb71b7e3d3076e902753e149bf
MOAAS <38762849+MOAAS@users.noreply.github.com>
2018-05-04 20:51:11 +0100

    LER E GUARDAR FICHEIROS!
    MUITAS FUNÇOES FIXES ADICIONADAS!
    Tambem mudei o nome de algumas variaveis por questões de clareza!!!!

--- Board.h
+++ Board.h
@@ -12,7 +12,7 @@ using namespace std;
 class Board
 {
 public:
-	Board(int x, int y);
+	Board(int size_x, int size_y);
 	void display() const;
 	void getWord();
 	bool Verify(int posX, int posY, char direction, string palavra); //Ve se pode por a palavra
@@ -21,11 +21,19 @@ public:
 	char ShowChar() const; //ve que letra esta na posicao do cursor
 	int CoordLimit() const; //da a coordenada maxima do board na direcao atual (usa o cursor)
 	bool isFull() const;
+	void clear(); // Limpa o board (Não usado de momento)
+	void clear(int size_x, int size_y); // Limpa o board, altera o tamanho.
 	void Fill();
+	void saveFile(string file_path);
+	void loadFile(string file_path);
 	vector<string> getPlacedWords() const;
 private:
 	vector<vector<char>> board;
-	int x, y;
+	int size_x, size_y;
 	map<string, string> placedWordsCoords; // "Palavra", "LCD"
 	Cursor Cursor;
 };
+
+int LCDtoPosX(string LCD); // recebe string da forma "LinhaColunaDireçao" devolve a coordenada X correspondente.
+int LCDtoPosY(string LCD); // recebe string da forma "LinhaColunaDireçao" devolve a coordenada Y correspondente.
+char LCDtoDir(string LCD);  // recebe string da forma "LinhaColunaDireçao" devolve a direçao correspondente.
