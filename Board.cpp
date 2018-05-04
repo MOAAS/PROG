9a5955ab5972a035fc795e69b53e30e457698f8c
MOAAS <38762849+MOAAS@users.noreply.github.com>
2018-05-04 20:53:10 +0100

    LER E GUARDAR FICHEIROS (IIIIIIMPLEEEMEEEEENTAÇÃO)
    novos nomes para algumas variaveis tambem
    corrigiiiiiidos erros muuuito gayys

--- Board.cpp
+++ Board.cpp
@@ -6,22 +6,23 @@
 #include <cstdlib>
 #include <windows.h>
 #include <string>
+#include <fstream>
 #include "consolecolors.h"
 #include "Dictionary.h"
 
-Board::Board(int x, int y) {
-	vector<vector<char>> v(x, vector<char>(y, '.'));
+Board::Board(int size_x, int size_y) {
+	vector<vector<char>> v(size_x, vector<char>(size_y, '.'));
 	board = v;
-	this->x = x;
-	this->y = y;
+	this->size_x = size_x;
+	this->size_y = size_y;
 }
 
 void Board::display() const {
-	const int N_ROWS = x;
-	const int N_COLUMNS = y;
+	const int N_COLUMNS = size_x;
+	const int N_ROWS = size_y;
 	char letras_cima = 'a'; // Limite superior terá letras minúsculas.
 	char letras_lado = 'A'; // Limite inferior terá letras maiúsculas
-	setcolor(LIGHTRED); // LIGHT RED
+	setcolor(LIGHTRED);
 	cout << "   ";
 	for (int i = 0; i < N_ROWS; i++) {
 		cout << letras_cima << ' ';
@@ -30,14 +31,14 @@ void Board::display() const {
 	cout << endl;
 	for (int i = 0; i < N_COLUMNS; i++) {
 		cout << letras_lado << "  ";
-		setcolor(BLACK, WHITE); // BLACK, WHITE
+		setcolor(BLACK, WHITE);
 		letras_lado++;
 		for (int j = 0; j < N_ROWS; j++)
 			cout << board[j][i] << ' ';
-		setcolor(LIGHTRED); // LIGHT RED
+		setcolor(LIGHTRED);
 		cout << endl;
 	}
-	setcolor(WHITE); // WHITE
+	setcolor(WHITE);
 }
 
 void Board::getWord()
@@ -47,7 +48,7 @@ void Board::getWord()
 	//CUIDADOS COM INPUT 
 	// '-' e '?'
 	string input;
-	cin >> input;
+	cin >> input; stringUpper(input); input[1] = tolower(input[1]); // Converte todos os carateres para maiusculas menos o segundo.
 	posY = (int)input[0] - (int)'A';
 	posX = (int)input[1] - (int)'a';
 	direction = input[2];
@@ -104,9 +105,8 @@ void Board::Insert(int posX, int posY, char direction, string word)
 	}
 	if (Cursor.MainCoord() < CoordLimit()) //verifica se pode por # depois
 		ChangeChar('#');
-	char LinColDir[3];
-	LinColDir[0] = posX + 'a'; 	LinColDir[1] = posY + 'A'; 	LinColDir[2] = direction;
-	placedWordsCoords[word] = LinColDir;
+	char LinColDir[4] = { posY + 'A', posX + 'a', direction, '\0' }; // LinColDir é uma string de 3 carateres que indica a linha a coluna e direçao da palavra.
+	placedWordsCoords[LinColDir] = word;
 }
 
 void Board::ChangeChar(char letra)
@@ -121,23 +121,23 @@ char Board::ShowChar() const
 int Board::CoordLimit() const
 {
 	if (Cursor.dir == 'H')
-		return x;
-	else return y;
+		return size_x;
+	else return size_y;
 }
 
 vector<string> Board::getPlacedWords() const {
 	vector<string> placedWords;
 	for (map<string, string>::const_iterator ite = placedWordsCoords.begin(); ite != placedWordsCoords.end(); ite++) {
-		placedWords.push_back(ite->first);
+		placedWords.push_back(ite->second);
 	}
 	return placedWords;
 }
 
 bool Board::isFull() const
 {
-	for (int i = 0; i < x; i++)
+	for (int i = 0; i < size_x; i++)
 	{
-		for (int j = 0; j < y; j++)
+		for (int j = 0; j < size_y; j++)
 			if (board[i][j] == '.')
 				return false;
 	}
@@ -146,10 +146,69 @@ bool Board::isFull() const
 
 void Board::Fill()
 {
-	for (int i = 0; i < x; i++)
+	for (int i = 0; i < size_x; i++)
 	{
-		for (int j = 0; j < y; j++)
+		for (int j = 0; j < size_y; j++)
 			if (board[i][j] == '.')
 				board[i][j] = '#';
 	}
+}
+
+void Board::saveFile(string file_path) {
+	ofstream file_dest(file_path);
+	for (int i = 0; i < size_x; i++) {
+		for (int j = 0; j < size_y; j++)
+			file_dest << board[j][i] << ' ';
+		file_dest << endl;
+	}
+	file_dest << endl << "WORD COORDINATES:" << endl;
+	for (map<string, string>::const_iterator ite = placedWordsCoords.begin(); ite != placedWordsCoords.end(); ite++)
+		file_dest << ite->first << " " << ite->second << endl;
+}
+
+void Board::loadFile(string file_path) {
+	ifstream file_orig(file_path); string line;
+	int size_x_file, size_y_file = -1; // Começa a -1 pois a linha extra abaixo do board incrementa o tamanho em 1 a mais.
+	if (file_orig.is_open()) {
+		getline(file_orig, line);
+		size_x_file = line.size() / 2; // 
+		while (line != "WORD COORDINATES:") {
+			getline(file_orig, line);
+			size_y_file++;
+		}
+		clear(size_x_file, size_y_file);
+		string word, coords; // Coords na forma LCD
+		while (file_orig >> coords) {
+			file_orig >> word;
+			Insert(LCDtoPosX(coords), LCDtoPosY(coords), LCDtoDir(coords), word);
+			// file_orig.clear(); file_orig.ignore(1000, '\n'); // Acho que isto nao e preciso. Deixar estar caso haja problemas relacionados.
+		}
+	}
+}
+
+void Board::clear() {
+	vector<vector<char>> v(size_x, vector<char>(size_y, '.'));
+	board = v;
+	placedWordsCoords.clear(); // limpa o map
+}
+
+void Board::clear(int new_size_x, int new_size_y) {
+	vector<vector<char>> v(new_size_x, vector<char>(new_size_y, '.'));
+	board = v;
+	this->size_x = new_size_x;
+	this->size_y = new_size_y;
+	placedWordsCoords.clear(); // limpa o map
+}
+
+
+int LCDtoPosX(string LCD) {
+	return LCD[1] - 'a';
+}
+
+int LCDtoPosY(string LCD) {
+	return LCD[0] - 'A';
+}
+
+char LCDtoDir(string LCD) {
+	return LCD[2];
 }
