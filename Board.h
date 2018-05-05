0399544fd7165dbb36359da968a73e5daaa0e48a
VextoN <38920805+VextoN@users.noreply.github.com>
2018-05-05 15:27:29 +0100

    OVERLOOOOOAD + DELETE

--- Board.h
+++ Board.h
@@ -1,3 +1,4 @@
+
 #pragma once
 #include <iostream>
 #include <vector>
@@ -15,14 +16,19 @@ public:
 	Board(int size_x, int size_y);
 	void display() const;
 	void getWord();
-	bool Verify(int posX, int posY, char direction, string palavra); //Ve se pode por a palavra
-	void Insert(int posX, int posY, char direction, string palavra); //mete
+	bool Verify(int posX, int posY, char direction, string palavra);
+	bool Verify(string coords, string palavra);//Ve se pode por a palavra
+	void Insert(int posX, int posY, char direction, string palavra);
+	void Insert(string coords, string palavra);//mete
+	void InsertAll();
+	bool Delete(string coords);
 	void ChangeChar(char letra); //altera no board a letra com base na posicao do cursor
 	char ShowChar() const; //ve que letra esta na posicao do cursor
 	int CoordLimit() const; //da a coordenada maxima do board na direcao atual (usa o cursor)
 	bool isFull() const;
-	void clear(); // Limpa o board (Não usado de momento)
-	void clear(int size_x, int size_y); // Limpa o board, altera o tamanho.
+	void clear();
+	void reset(); // Limpa o board (Não usado de momento)
+	void reset(int size_x, int size_y); // Limpa o board, altera o tamanho.
 	void Fill();
 	void saveFile(string file_path);
 	void loadFile(string file_path);
@@ -36,4 +42,4 @@ private:
 
 int LCDtoPosX(string LCD); // recebe string da forma "LinhaColunaDireçao" devolve a coordenada X correspondente.
 int LCDtoPosY(string LCD); // recebe string da forma "LinhaColunaDireçao" devolve a coordenada Y correspondente.
-char LCDtoDir(string LCD);  // recebe string da forma "LinhaColunaDireçao" devolve a direçao correspondente.
+char LCDtoDir(string LCD); // recebe string da forma "LinhaColunaDireçao" devolve a direçao correspondente.
