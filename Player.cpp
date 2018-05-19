0e32b09f9d47b6d4f1a3d14cddcec5d57638f29f
MOAAS <38762849+MOAAS@users.noreply.github.com>
2018-05-19 01:58:02 +0100

    final cleanup

--- Player.cpp
+++ Player.cpp
@@ -3,30 +3,37 @@
 
 
 Player::Player() {
+	normalMode = true;
 	numClues = 0;
 }
 
 Player::Player(string name) {
+	normalMode = true;
 	numClues = 0;
 	this->name = name;
 }
 
-string Player::getName() const
-{
+string Player::getName() const {
 	return name;
 }
 
-size_t Player::getNumClues()
-{
+size_t Player::getNumClues() const {
 	return numClues;
+}
+
+bool Player::isNormalMode() const {
+	return normalMode;
 }
 
 void Player::setName(string name) {
 	this->name = name;
 }
 
-void Player::incClues()
-{
+void Player::setEasyMode() {
+	normalMode = false;
+}
+
+void Player::incClues() {
 	numClues++;
 }
 
@@ -34,8 +41,8 @@ void Player::startClock() {
 	startTime = time(NULL);
 }
 
+
 size_t Player::endClock() {
 	endTime = time(NULL);
 	return endTime - startTime;
 }
-
